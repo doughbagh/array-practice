0881ff64ecc857a659759944b5e10034496ebc64
doughbagh <lukas@senkuscloud.com>
2025-01-19 00:14:15 +0000

    Add files via upload

---
+++ Array.py
@@ -0,0 +1,10 @@
+array_list = [10, 7, 5, 8, 11, 9] # Array list
+
+# Output all array values:
+for number in array_list:
+    print(number)
+
+# Looking for value 5 in the array list:
+for i in range(len(array_list)):
+    if array_list[i] == 5:
+        print("Value 5 found at index:", i) # Output the index of value 5
